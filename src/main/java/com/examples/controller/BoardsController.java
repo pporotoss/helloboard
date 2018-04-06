aeb3a88439f7d6b39a19236426e59b664b9bd87c
Kwon <pporotoss@gmail.com>
2018-04-06 15:26:29 +0900

    메인페이지 매핑 주소 / -> "" 로 변경

--- src/main/java/com/examples/controller/BoardsController.java
+++ src/main/java/com/examples/controller/BoardsController.java
@@ -5,8 +5,6 @@ import com.examples.dto.Pagenation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Comparator;
-
 @Controller
 @RequestMapping("boards")
 public class BoardsController {
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>index.jsp!!!!!!!!!!!</h1>
+</body>
+</html>
